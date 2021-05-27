69f41129cf1b38fb09741b281ff23a2525f98319
greg mathias <mathiasgreg@gmail.com>
2021-05-27 18:08:53 -0400

    xy

---
+++ xy.js
@@ -0,0 +1,19 @@
+xy = () => {
+	let list = []
+	for (let y = 0; y < 5; y++) {
+		for (let x = 0; x < 5; x++) {
+			list.push(x)
+			list.push(y)
+		}
+	
+	}
+
+	return list
+
+}
+
+console.log(xy())
+
+
+
+
---
+++ xy.py
@@ -0,0 +1,9 @@
+def main():
+    patch = []
+    for y in range(5):
+        for x in range(5):
+            patch.append(x)
+            patch.append(y)
+            print(patch)
+
+main()
