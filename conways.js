d75f7628e8bdc58adca499d3653d89f1b5f5ac52
greg mathias <mathiasgreg@gmail.com>
2021-06-14 22:50:55 -0400

    conwaying

---
+++ conways.js
@@ -0,0 +1,22 @@
+import random, time, copy
+WIDTH = 60
+HEIGHT = 20
+
+# Create list of lists for the cells
+next_cells = []
+for x in range(WIDTH):
+    column = [] # Create a new column
+    for y in range(HEIGHT):
+        if random.randint(0, 1) == 0:
+            column.append('#') # Add a living cell
+        else:
+            column.append(' ') # Add a dead cell
+    next_cells.append(column) # next_cells is a list of column lists 
+    print(column)
+    print(next_cells)
+    print(len(column), len(next_cells))
+
+let nextCells = []
+for(let x = 0; x < len(width); x++) {
+	let column = []
+	for(let y = 0; y < len(height); y++) {
