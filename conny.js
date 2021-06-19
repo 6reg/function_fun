3804a836cc75f2f62e0dfb4601db97c7fef140cf
greg mathias <mathiasgreg@gmail.com>
2021-06-18 23:53:24 -0400

    conny

---
+++ conny.js
@@ -0,0 +1,5 @@
+const apple = ["a", "b", "c"]
+
+const apple1 = (x) => x.pop("d")
+
+console.log(apple1(apple))
---
+++ conny.py
@@ -0,0 +1,7 @@
+my_data = {["hi", "hello"], ["bye", "byebye"]}
+
+def do_sumn(stuff):
+    stuff.append("he")
+    return stuff
+
+print(do_sumn(my_data))
